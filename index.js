0e7d96b186959bf9f575e37d7639b6ae76f949d8
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-17 07:00:54 +0530

    completed jokeBot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config();
 
@@ -7,9 +8,31 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on("message", (msg) => {
+//   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Received your message");
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(chatId, "Received your message");
+// });
+
+// Matches "/joke"
+bot.onText(/\/joke/, async (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const response = await axios.get(
+    "http://www.official-joke-api.appspot.com/random_joke"
+  );
+
+  const setup = response.data.setup;
+  const punchLine = response.data.punchline;
+
+  const chatId = msg.chat.id;
+  //   const resp = match[1]; // the captured "whatever"
+
+  // reply with joke
+  bot.sendMessage(chatId, setup + "\n" + punchLine);
 });
